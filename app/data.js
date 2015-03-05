0b260ebf5c97d9e9c50e346b8e8ec36d6cea005b
mrelf <crisan_andreidan@yahoo.com>
2015-03-05 13:45:46 +0200

    Revert "alo"
    This reverts commit 8d6b5fbfa3b613176ccf3c4deffb44a77082055a.

--- app/data.js
+++ app/data.js
@@ -6,8 +6,8 @@ taskManagerData.submenuItemsData = [
         {itemUrl: '#', itemText: 'Item Three'}
 ];
 taskManagerData.mainMenuItemsData = [
-    {itemUrl: '/', itemIcon: 'addIcon', itemText: 'Add / Create'},
-    {itemUrl: 'in-progress', itemIcon: 'workIcon', itemText: 'In progress'},
-    {itemUrl: 'parked-out', itemIcon: 'inactiveIcon', itemText: 'Parked Out'},
-    {itemUrl: 'backlog', itemIcon: 'folderIcon', itemText: 'Backlog'},
+    {itemUrl: '/', itemIcon: 'addIcon', itemText: 'Add / Create', itemMo: 'aa'},
+    {itemUrl: 'in-progress', itemIcon: 'workIcon', itemText: 'In progress', itemMo: ''},
+    {itemUrl: 'parked-out', itemIcon: 'inactiveIcon', itemText: 'Parked Out', itemMo: ''},
+    {itemUrl: 'backlog', itemIcon: 'folderIcon', itemText: 'Backlog', itemMo: ''},
 ];
